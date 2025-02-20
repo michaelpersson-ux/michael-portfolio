dd6789402561f3d074b908506eaafd7a2b103ef8
Michael Persson <michael.persson86@gmail.com>
2025-02-20 04:02:00 +0100

    dark mode + formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Image from "next/image";
-import { Button } from "../components/ui/button";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { setTheme } = useTheme();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -25,8 +28,12 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Button variant="destructive">Click me</Button>
-          <Button variant="destructive">Click me</Button>
+          <Button onClick={() => setTheme("light")} variant="default">
+            Click me
+          </Button>
+          <Button onClick={() => setTheme("dark")} variant="secondary">
+            Click me
+          </Button>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
