791379a176b265cc831f33804050dc9ed2ebc482
Michael Persson <michael.persson86@gmail.com>
2025-02-21 21:09:28 +0100

    .lottie issue on dev server

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   /* config options here */
   //output: "export",
   assetPrefix: "./",
+  experimental: {
+    turbo: {
+      // ...
+    },
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.lottie$/,
+      type: "asset/resource",
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
