3b0a392ce9a0d220e4d6b37bcfb9bb70cf469da9
Michael Persson <michael.persson86@gmail.com>
2025-02-27 03:28:11 +0100

    fixed dotlottie issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Player from "@/components/lottie-player";
 export default function Home() {
   return (
     <div className="flex flex-col md:flex-row justify-start md:justify-center items-center min-h-0 md:min-h-[calc(100vh-80px)] gap-6 pt-8 md:pt-0 animate-in fade-in-0 px-8 linear-ease duration-1000">
-      <div className="flex grow basis-1 flex-col gap-6">
+      <div className="flex grow basis-1 flex-col gap-8">
         <div className="flex flex-col gap-4">
           <p className="text-3xl lg:text-5xl italic">Hello, my name is</p>
           <p className="text-7xl font-bold">Michael Persson</p>
@@ -15,13 +15,20 @@ export default function Home() {
           I&apos;m a Trondheim-based <b>UX engineer</b> passionate about
           creating beautiful and functional user experiences.
         </p>
-        <Button asChild variant="default" size="lg" className="w-fit">
-          <Link href="/about" rel="noopener noreferrer">
-            About me
-          </Link>
-        </Button>
+        <div className="flex gap-2">
+          <Button asChild variant="default" size="lg" className="w-fit">
+            <Link href="/about" rel="noopener noreferrer">
+              Profile
+            </Link>
+          </Button>
+          <Button asChild variant="secondary" size="lg" className="w-fit">
+            <Link href="/case-studies" rel="noopener noreferrer">
+              Case studies
+            </Link>
+          </Button>
+        </div>
       </div>
-      <div className="flex grow max-w-[200px] md:max-w-sm basis-1">
+      <div className="flex grow md:max-w-sm lg:h-[400px] basis-1">
         <Player />
       </div>
     </div>
