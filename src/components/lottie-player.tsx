12fcb5b62849c408f1a0c68a93312438cf474c7e
Michael Persson <michael.persson86@gmail.com>
2025-02-28 01:54:22 +0100

    commit

---
+++ src/components/lottie-player.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { LoaderCircle } from "lucide-react";
+import pig from "../../public/pig.lottie";
+
+const Player = () => {
+  const [lottieLoaded, setLottieLoaded] = React.useState(true);
+
+  return (
+    <>
+      <div
+        className={
+          "animate-in fade-in-0 linear-ease duration-1000" +
+          (lottieLoaded ? "" : " hidden")
+        }
+      >
+        <DotLottieReact
+          src={pig}
+          loop
+          autoplay
+          renderConfig={{ autoResize: true }}
+        />
+      </div>
+      <div
+        className={
+          "flex grow justify-center items-center h-full" +
+          (lottieLoaded ? " hidden" : "")
+        }
+      >
+        <LoaderCircle className="animate-spin" />
+      </div>
+    </>
+  );
+};
+export default Player;
